1873aa28482fa3f99215217bfa5f205f2fe411b9
Firas Yahyaoui <firas.yahyaoui@imt-atlantique.net>
2024-01-23 17:09:19 +0100

    add: footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import CustomNavbar from "./components/CustomNavbar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ function App() {
       <Container>
         <Outlet />
       </Container>
+      <Footer />
     </QueryClientProvider>
   );
 }
