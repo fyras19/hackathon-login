abe0e2c02feb5dc1aa6c63337e265e7627ecd1a8
moDall <mohamed_dallel@outlook.com>
2024-01-25 23:01:31 +0100

    clean the code add tests

--- server/app.py
+++ server/app.py
@@ -13,56 +13,80 @@ basis_url = "https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/2
 
 with app.app_context():
     theme_cat_dict = jsonify(get_theme_cat_dict())
-    print(theme_cat_dict)
+
+# Default route, renders the index.html template
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+
+# Route to retrieve theme category dictionary
 @app.route('/get_theme_cat_dict/', methods=['GET'])
 def get_theme_categ_dict():
     return make_response(theme_cat_dict, 200)
 
+
+# Route to retrieve all themes
 @app.route('/get_theme/', methods=['GET'])
 def get_theme():
     return make_response(jsonify(get_theme_names()), 200)
 
+
+# Route to retrieve all organizers
 @app.route('/get_organizer/', methods=['GET'])
 def get_organizer():
     return make_response(jsonify(get_organizer_names()), 200)
 
+
+# Route to retrieve all neighborhoods
 @app.route('/get_hood/', methods=['GET'])
 def get_hood():
     return make_response(jsonify(get_hood_names()), 200)
 
+
+# Route to retrieve all cities
 @app.route('/get_city/', methods=['GET'])
 def get_city():
     return make_response(jsonify(get_city_names()), 200)
 
+
+# Route to retrieve all events.
 @app.route('/all_events', methods=['GET'])
 def get_events_by_offset_and_limit():
+    """Get all events."""
     offset = request.args.get('offset', default=0, type=int)
     limit = request.args.get('limit', default=20, type=int)
     url = basis_url + f"records?limit={limit}&offset={offset}"
     response = requests.get(url)
     return make_response(response.json(), response.status_code)
 
+
+# Route to retrieve events by theme.
 @app.route('/get_event_by_theme/<theme>', methods=['GET'])
 def get_event_by_theme(theme):
+    """Get events by theme"""
     offset = request.args.get('offset', default=0, type=int)
     limit = request.args.get('limit', default=20, type=int)
     url = basis_url +  f"records?refine=rubrique%3A%22{theme}%22"
     response = requests.get(url, params={'offset': offset, 'limit': limit})
     return make_response(response.json(), response.status_code)
 
+
+# Route to retrieve events by city
 @app.route('/get_event_by_city/<city>')
 def get_event_by_city(city):
+    """Get events by city."""
     offset = request.args.get('offset', default=0, type=int)
     limit = request.args.get('limit', default=20, type=int)
     url = basis_url + f"records?refine=ville%3A%22{city}%22"
     response = requests.get(url, params={'offset': offset, 'limit': limit})
     return make_response(response.json(), response.status_code)
 
+
+# Route to retrieve filtered events with pagination support
 @app.route('/get_filtered_events/', methods=['POST'])
 def get_filtered_events():
+    """Get filtered events with pagination support."""
     offset = request.args.get('offset', default=0, type=int)
     limit = request.args.get('limit', default=20, type=int)
 
@@ -73,7 +97,6 @@ def get_filtered_events():
     # Build the API query parameters based on the provided filters
     query_params = [('offset', offset), ('limit', limit)]
     body = request.json
-    print(body)
     if (body["order_by"]) :
         query_params.append(('order_by', body["order_by"]))
     if (body["only_upcoming_events"]) :
@@ -84,7 +107,6 @@ def get_filtered_events():
                 query_params.append(('refine', f'{filter_name}:"{filter_value}"'))
     # Make a request to the API with the filtered parameters
     url  = basis_url + "records"
-    print(query_params)
     api_response = requests.get(url, params=query_params)
 
     # Check if the request was successful (status code 200)
@@ -95,8 +117,11 @@ def get_filtered_events():
         # Return an error message if the request was not successful
         return jsonify({"error": "Failed to fetch events from the API"}), 500
 
+
+# Route to retrieve event by ID
 @app.route('/get_event_by_id/<id>', methods=['GET'])
 def get_event_by_id(id):
+    """Get event by ID."""
     param_list = id.split("_")
     id_manif = int(param_list[0])
     date= param_list[1]
@@ -118,8 +143,11 @@ def get_event_by_id(id):
         # Return an error message if the request was not successful
         return jsonify({"error": "Failed to fetch events from the API"}), 500
 
+
+# Route to retrieve postal address based on coordinates
 @app.route('/get_adresse_postale/', methods=['GET'])
 def get_adresse_postale():
+    """Get postal address based on coordinates."""
     lon = request.args.get('lon', type=float, default=0)
     lat = request.args.get('lat', type=float, default=0)
     if lat==0 or lon==0 :
@@ -141,6 +169,8 @@ def get_adresse_postale():
         # Return an error message if the request was not successful
         return jsonify({"error": "Failed to fetch information from the API"}), 500
 
+
+# Main entry point of the application
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
---
+++ server/backend_test.py
@@ -0,0 +1,81 @@
+# content of test_sample.py
+import pytest
+import json
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        with app.app_context():
+            yield client
+
+def test_hello(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_get_theme_cat_dict(client):
+    response = client.get('/get_theme_cat_dict/')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, dict)
+    assert all(isinstance(categories, list) for categories in data.values())
+
+def test_get_theme(client):
+    response = client.get('/get_theme/')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, list)
+
+def test_get_organizer(client):
+    response = client.get('/get_organizer/')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, list)
+
+def test_get_hood(client):
+    response = client.get('/get_hood/')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, list)
+
+def test_get_city(client):
+    response = client.get('/get_city/')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, list)
+
+def test_all_events(client):
+    response = client.get('/all_events')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, dict)
+
+def test_get_event_by_theme(client):
+    response = client.get('/get_event_by_theme/Culture')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, dict)
+
+def test_get_event_by_city(client):
+    response = client.get('/get_event_by_city/Nantes')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, dict)
+
+def test_get_filtered_events(client):
+    response = client.post('/get_filtered_events/', json={'filters': {}, 'order_by': 'date', 'only_upcoming_events': True})
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, dict)
+
+def test_get_event_by_id(client):
+    response = client.get('/get_event_by_id/57559_2024-02-04_15:00')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, dict)
+
+def test_get_adresse_postale(client):
+    response = client.get('/get_adresse_postale/?lon=0&lat=0')
+    assert response.status_code == 400
+    data = json.loads(response.data)
+    assert "error" in data
